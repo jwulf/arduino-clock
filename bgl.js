fcafc5438fafa10bcaaebbda1af7e2e3a8936a7e
Josh Wulf <josh@magikcraft.io>
2019-02-04 19:23:20 +1000

    Add BGL

---
+++ bgl.js
@@ -0,0 +1,61 @@
+var arrows = {
+    SingleDown: '↓︎',
+    DoubleDown: '↓︎↓︎',
+    SingleUp: '↑',
+    DoubleUp: '↑↑',
+    FortyFiveDown: '↘',
+    FortyFiveUp: '↗',
+    Flat: '→'
+};
+
+// Set to false to use the US units dg/L. Set to true to use EU/AU/NZ units mmol/L
+var mmol = true
+
+// If the Nightscout data is older than this, we'll change its color
+var nightscoutDataWindowTolerance = 7 //mins
+
+function updateBglFromNightscout() {
+    axios.get(nightscoutUrl, { crossdomain: true }).then(res => {
+        try {
+            console.log('Got CGM update');
+            var data = res.data[0];
+            var bgl = data.sgv;
+
+            // Check if Nightscout data is more than 5 mins old
+            var dateString = data.dateString;
+            var timestamp = new Date(dateString);
+            var now = new Date();
+            dataAgeMinutes = (now - timestamp) / 60 / 1000;
+            console.log({dataAgeMinutes})
+            var dataIsOld = dataAgeMinutes > nightscoutDataWindowTolerance
+            clock.bglOutOfDate = dataIsOld;
+
+            if (mmol) {
+                bgl = bgl / 18;
+            }
+            var arrow = arrows[data.direction] || '';
+            resolve(round(bgl, 1) + ' ' + arrow);
+        } catch (e) {
+            // This will happen if Nightscout returns no BGL data
+            console.log(e);
+            resolve('BGL n/a');
+        }
+    }).catch(console.log);
+}
+
+/* Utility functions */
+
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
+function zeroPadding(num, digit) {
+    var zero = '';
+    for (var i = 0; i < digit; i++) {
+        zero += '0';
+    }
+    return (zero + num).slice(-digit);
+}
+
+module.exports = updateBglFromNightscout;
--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const dayjs = require('dayjs')
 const os = require('os');
 const localtunnel = require('localtunnel');
 const LCD = require('./lcd');
+const updateBglFromNightscout = require('./bgl');
 
 const port = 3000;
 
 console.log('Starting j5...');
+
+// Poll Nightscout every how many seconds?
+var nightscoutPollInterval = 5 // seconds
 
 function printTime(h, m, s) {
     const pad = n => n.toString().padStart(2, '0');
@@ -20,15 +24,29 @@ function printTime(h, m, s) {
 }
 
 setInterval(() => {
+    const {h,m,s} = getTime();
+    printTime(h, m, s);
+}, 1000);
+
+setInterval(async () => {
+    const {h,m,s} = getTime();
+    if (h === 8 && m === 59) {
+        return;
+    }
+    const bgl = await updateBglFromNightscout();
+    const today = dayjs().format('ddd D MMM');
+    LCD.print(`${today} ${bgl}`, 1);
+}, nightscoutPollInterval);
+
+function getTime() {
     const date = new Date();
     const h = date.getHours();
     const m = date.getMinutes();
     const s = date.getSeconds();
-    printTime(h, m, s);
-}, 1000);
-
-
-
+    return {
+        h, m, s
+    };
+}
 
 var tunnel = localtunnel(port, function(err, tunnel) {
     if (err) {
